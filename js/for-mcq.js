5bf4284a0af186e31a7a9e463d00bfed3f344bb2
mold-skill <login@msmahato.com.np>
2023-06-13 15:19:05 +0545

    MCQ almost ready

--- js/for-mcq.js
+++ js/for-mcq.js
@@ -1,36 +1,53 @@
-$(document).ready(function () {
-  // Keep track of correct answers and total marks
-  var correctAnswers = 0;
-  var totalMarks = 0;
+function checkAnswers() {
+  var score = 0;
 
-  // Function to handle the form submission
-  $("form").on("submit", function (e) {
-    e.preventDefault(); // Prevent form submission
+  var form = document.getElementById("quizForm");
+  var resultDiv = document.getElementById("result");
+  resultDiv.innerHTML = "";
 
-    // Loop through each question
-    $(".question").each(function (index) {
-      var selectedOption = $(this).find(":checked").val();
-      var correctOption = $(this).data("correct-option");
-      var marks = $(this).data("marks");
+  var questions = form.getElementsByClassName("question");
+  for (var i = 0; i < questions.length; i++) {
+    var question = questions[i];
+    var options = question.getElementsByTagName("input");
+    var selectedOption = getSelectedOption(options);
 
-      // Increment total marks
-      totalMarks += marks;
+    if (selectedOption && selectedOption.dataset.correct === "true") {
+      score++;
+    } else if (selectedOption) {
+      var correctOption = question.querySelector("input[data-correct='true']");
+      var correctAnswer = getOptionLabel(correctOption);
+      var incorrectAnswer = getOptionLabel(selectedOption);
 
-      // Check if the selected option is correct
-      if (selectedOption == correctOption) {
-        correctAnswers++;
-      }
+      var incorrectAnswerDiv = document.createElement("div");
+      incorrectAnswerDiv.innerHTML = "Incorrect Answer. Correct Answer: " + correctAnswer + ". Your Answer: " + incorrectAnswer;
+      incorrectAnswerDiv.style.color = "red";
+      question.parentNode.insertBefore(incorrectAnswerDiv, question.nextSibling);
+    } else {
+      var correctOption = question.querySelector("input[data-correct='true']");
+      var correctAnswer = getOptionLabel(correctOption);
 
-      // Display the correct option
-      $(this)
-        .find(".correct-answer")
-        .text("Correct answer: Option " + correctOption);
-      $(this).find(".correct-answer").show();
-    });
+      var correctAnswerDiv = document.createElement("div");
+      correctAnswerDiv.innerHTML = "Correct Answer: " + correctAnswer;
+      question.parentNode.insertBefore(correctAnswerDiv, question.nextSibling);
+    }
+  }
 
-    // Display the marks
-    $("#marks").text("You scored " + correctAnswers + " out of " + totalMarks + " marks");
-    $("#marks").show();
-    $("#submit").attr("disabled", true); // Disable the submit button
-  });
-});
+  var percentage = (score / questions.length) * 100;
+  resultDiv.innerHTML = "Your score: " + score + "/" + questions.length + " (" + percentage + "%)";
+
+  // Disable the submit button
+  document.getElementById("submit").disabled = true;
+}
+
+function getSelectedOption(options) {
+  for (var i = 0; i < options.length; i++) {
+    if (options[i].checked) {
+      return options[i];
+    }
+  }
+  return null;
+}
+
+function getOptionLabel(option) {
+  return option.nextSibling.textContent.trim();
+}
