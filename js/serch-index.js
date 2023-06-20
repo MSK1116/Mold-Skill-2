519b03341d7b21edc4d61ece67e57170b2519896
MSK1116 <anishkushwaha1116@gmail.com>
2023-06-20 10:00:51 +0545

    working on search bar

--- js/serch-index.js
+++ js/serch-index.js
@@ -5,8 +5,8 @@ function performSearch() {
 }
 
 function searchFunction(query) {
-  // books
   var data = [
+    // books
     {title: "Concept of Physics-2", url: "Physics Book/Concept of Phy 2.html"},
     {title: "Concept of Physics-1", url: "Physics Book/Concepts Of Phy 1.html"},
     {title: "For the Love of Physics", url: "Physics Book/For the Love of Physics.html"},
@@ -16,7 +16,23 @@ function searchFunction(query) {
     {title: "Physics Books for grade xi & xii", url: "Physics Book/phy-book.html"},
     {title: "University Physics solution", url: "Physics Book/University Physics solution.html"},
     {title: "University Physics 13 edition", url: "Physics Book/University-Physics-13edt.html"},
-    // ... more data
+    // lab file
+    {title: "Chemistry Lab file for class 11", url: "1.CHEMISTRY/Grade XI lab file/chm-lab-xi.html"},
+    {title: "To separate the volatile and non-volatile substance from mixture of sand and camphor by sublimation.", url: "1.CHEMISTRY/Grade XI lab file/Exp No-1.html"},
+    {title: "To seperate the sand and salt from the mixture by filteration.", url: "1.CHEMISTRY/Grade XI lab file/Exp No-2.html"},
+    {title: "To seperate the mixture of insoluble solid.", url: "1.CHEMISTRY/Grade XI lab file/Exp No-3.html"},
+    {title: "To prepare a saturated solution of impure salt and obtain the pure salt by crystallization.", url: "1.CHEMISTRY/Grade XI lab file/Exp No-4.html"},
+    {title: "To neutralize sodium hydroxide with hydrochloric acid and recover crystal of sodium chloride", url: "1.CHEMISTRY/Grade XI lab file/Exp No-5.html"},
+    {title: "To test the ferrous ion in the aq solution and oxidize it to ferric ion by redox reaction.", url: "1.CHEMISTRY/Grade XI lab file/Exp No-6.html"},
+    {title: "To obtain the pure water from given sampe of impure water by the process of distillation.", url: "1.CHEMISTRY/Grade XI lab file/Exp No-7.html"},
+    {title: "To detect the radical present in salt sample by dry & wet test. (a1)", url: "1.CHEMISTRY/Grade XI lab file/Exp No-8.html"},
+    {title: "To detect the radical present in salt sample by dry & wet test. (a2)", url: "1.CHEMISTRY/Grade XI lab file/Exp No-9.html"},
+    {title: "To detect the radical present in salt sample by dry & wet test. (a3)", url: "1.CHEMISTRY/Grade XI lab file/Exp No-10.html"},
+    {title: "To detect the basic radical in the given salt sample by dry & wet test (b1)", url: "1.CHEMISTRY/Grade XI lab file/Exp No-11.html"},
+    {title: "To detect the basic radical in the given salt sample by dry & wet test (b2)", url: "1.CHEMISTRY/Grade XI lab file/Exp No-12.html"},
+    {title: "To detect the basic radical in the given salt sample by dry & wet test (b3)", url: "1.CHEMISTRY/Grade XI lab file/Exp No-13.html"},
+    {title: "To prepare hydrogen gas and study it's properties.", url: "1.CHEMISTRY/Grade XI lab file/Exp No-14.html"},
+    {title: "To prepare Ammonia NH3 gas and study it's properties.", url: "1.CHEMISTRY/Grade XI lab file/Exp No-15.html"},
   ];
 
   var filteredResults = data.filter(function (item) {
@@ -48,7 +64,7 @@ function displayResults(results) {
 }
 
 document.addEventListener("DOMContentLoaded", function (event) {
-  var searchForm = document.querySelector(".navbar-search");
+  var searchForm = document.getElementById("searchForm");
   var searchInput = document.getElementById("searchInput");
 
   searchForm.addEventListener("submit", function (event) {
@@ -63,7 +79,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
     }
   });
 
-  var searchButton = document.getElementById("searchDropdown-");
+  var searchButton = document.getElementById("searchButton");
   searchButton.addEventListener("click", function (event) {
     event.preventDefault();
     performSearch();
