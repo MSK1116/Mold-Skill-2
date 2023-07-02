ad8479d207e0461513f7a4d2bc89f1eabde8d7d3
MSK1116 <anishkushwaha1116@gmail.com>
2023-07-02 09:16:40 +0545

    commit the suffle

--- js/for-mcq.js
+++ js/for-mcq.js
@@ -1,3 +1,51 @@
+function shuffleArray(array) {
+  var currentIndex = array.length;
+  var temporaryValue, randomIndex;
+
+  // While there are elements to shuffle
+  while (currentIndex !== 0) {
+    // Pick a remaining element
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // Swap the current element with the random element
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+function shuffleQuestionsAndOptions() {
+  var form = document.getElementById("quizForm");
+  var questions = Array.from(form.getElementsByClassName("question"));
+  questions = shuffleArray(questions);
+
+  // Create a temporary container
+  var container = document.createElement("div");
+
+  for (var i = 0; i < questions.length; i++) {
+    var question = questions[i];
+    var options = Array.from(question.getElementsByTagName("input"));
+    options = shuffleArray(options);
+
+    for (var j = 0; j < options.length; j++) {
+      container.appendChild(options[j].parentNode);
+    }
+
+    container.appendChild(question);
+  }
+
+  // Clear the form
+  form.innerHTML = "";
+
+  // Append the shuffled elements back to the form
+  while (container.firstChild) {
+    form.appendChild(container.firstChild);
+  }
+}
+
 function checkAnswers() {
   var score = 0;
 
@@ -11,10 +59,10 @@ function checkAnswers() {
     var options = question.getElementsByTagName("input");
     var selectedOption = getSelectedOption(options);
 
-    if (selectedOption && selectedOption.dataset.correct === "true") {
+    if (selectedOption && selectedOption.dataset.correct === "t") {
       score++;
     } else if (selectedOption) {
-      var correctOption = question.querySelector("input[data-correct='true']");
+      var correctOption = question.querySelector("input[data-correct='t']");
       var correctLabel = correctOption.nextElementSibling;
       var correctAnswer = getOptionLabel(correctLabel);
 
@@ -44,3 +92,6 @@ function getSelectedOption(options) {
 function getOptionLabel(option) {
   return option.textContent.trim();
 }
+
+// Shuffle the questions and options on page load
+shuffleQuestionsAndOptions();
